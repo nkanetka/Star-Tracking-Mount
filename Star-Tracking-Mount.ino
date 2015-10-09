9165af1835b5c13a2cd5b4e564c7e91782d3adad
Nehal Kanetkar <nkanetkar@pelmorex.com>
2015-10-08 23:17:32 -0400

    Modifying the Arduino code to work with buttons

--- Star-Tracking-Mount.ino
+++ Star-Tracking-Mount.ino
@@ -1,29 +1,60 @@
 #include <SoftwareSerial.h>
 #include <Stepper.h>
 
+// Constants and variables
 int delayLength = 1353; // In microseconds 450721
-int reverseLength = 300; // In seconds
+int reverseLength = 30; // In seconds
 int chA = 12; // setting the Channel A pin
 int chB = 13; // setting the Channel B pin
 int brakeA = 9; // setting the brake for channel A
 int brakeB = 8; // setting the brake for channel B
-bool reverse = false;
+volatile bool up = false;
+volatile bool reverse = false;
+volatile bool stopMotor = false;
+int buttonForward = 6; // green button | pin 6
+int buttonStop = 3; // red button | pin 3
+int buttonBackward = 4; // black button | pin 4
+volatile int state = LOW; // CHANGE!
 
 void setup() {
-  // put your setup code here, to run once:
+  // Outputs for the stepper motor
   pinMode(chA, OUTPUT);
   pinMode(chB, OUTPUT);
-  
+
+  // Output for the stopping of the stepper motor
   pinMode(brakeA, OUTPUT);
   pinMode(brakeB, OUTPUT);
+
+  // Input for the control buttons
+  pinMode(buttonForward, INPUT); // Setting the forward button
+  pinMode(buttonStop, INPUT); // Setting the stop button
+  pinMode(buttonBackward, INPUT); // Setting the backward button
+//  attachInterrupt(digitalPinToInterrupt(buttonForward), setDirectionForwards, LOW); // green button
+  attachInterrupt(digitalPinToInterrupt(buttonStop), stopFunction, RISING); // red button
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (reverse) {
+//  Serial.println(digitalRead(buttonForward));
+//  Serial.println(digitalRead(buttonBackward));
+//  Serial.println(digitalRead(buttonStop));
+  if (digitalRead(buttonBackward) == HIGH){
+    up = false;
+    stopMotor = false;
     backward();
-  } else {
+  }
+  
+  if(digitalRead(buttonForward) == HIGH || up) {
+    stopMotor = false;
+    reverse = false;
+    up = true;
     forward();
+  } 
+  
+  if(stopMotor) {
+//    up = false;
+//    reverse = false;
+//    stopTurning();
   }
 }
 
@@ -32,14 +63,14 @@ void forward() {
    digitalWrite(brakeA, LOW);
    digitalWrite(brakeB, HIGH);
    digitalWrite(chB, HIGH);
-   analogWrite(3, 255);
+   analogWrite(5, 255);
    delay(delayLength);
    
  // 2
    digitalWrite(brakeA, LOW);
    digitalWrite(brakeB, HIGH);
    digitalWrite(chB, LOW);
-   analogWrite(3, 255);
+   analogWrite(5, 255);
    delay(delayLength);
    
  // 3
@@ -62,14 +93,14 @@ void backward() {
    digitalWrite(brakeA, LOW);
    digitalWrite(brakeB, HIGH);
    digitalWrite(chB, LOW);
-   analogWrite(3, 255);
+   analogWrite(5, 255);
    delay(reverseLength);
    
  // 2
    digitalWrite(brakeA, LOW);
    digitalWrite(brakeB, HIGH);
    digitalWrite(chB, HIGH);
-   analogWrite(3, 255);
+   analogWrite(5, 255);
    delay(reverseLength);
    
  // 3
@@ -86,3 +117,46 @@ void backward() {
    analogWrite(11, 255);
    delay(reverseLength);
 }
+
+void stopTurning() {
+  // 1
+   digitalWrite(brakeA, LOW);
+   digitalWrite(brakeB, LOW);
+   digitalWrite(chB, LOW);
+   analogWrite(5, 0);
+   delay(delayLength);
+   
+ // 2
+   digitalWrite(brakeA, LOW);
+   digitalWrite(brakeB, LOW);
+   digitalWrite(chB, LOW);
+   analogWrite(5, 0);
+   delay(delayLength);
+   
+ // 3
+   digitalWrite(brakeA, LOW);
+   digitalWrite(brakeB, LOW);
+   digitalWrite(chA, LOW);
+   analogWrite(11, 0);
+   delay(delayLength);
+
+ // 4
+   digitalWrite(brakeA, LOW);
+   digitalWrite(brakeB, LOW);
+   digitalWrite(chA, LOW);
+   analogWrite(11, 0);
+   delay(delayLength);
+}
+
+void setDirectionForwards() {
+  reverse = false;
+  stopMotor = false;
+  up = true;
+}
+
+void stopFunction() {
+  reverse = false;
+  stopMotor = true;
+  up = false;
+}
+
